e06a417d06759cf983e6b14d2374b34b0b5a6f55
Markus Reinhold <markusreinhold@icloud.com>
2017-08-19 21:01:33 +0200

    Set up unit test

---
+++ tests/unit/NchanTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Marein\Nchan;
+
+use PHPUnit\Framework\TestCase;
+
+class NchanTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function dummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
