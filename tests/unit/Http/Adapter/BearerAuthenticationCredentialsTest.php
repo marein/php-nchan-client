435b63453a17d46412fc5e9531a9225acd983f8c
Markus Reinhold <markusreinhold@icloud.com>
2017-08-20 12:20:34 +0200

    Add unit test for BearerAuthenticationCredentials

---
+++ tests/unit/Http/Adapter/BearerAuthenticationCredentialsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Marein\Nchan\Http\Adapter;
+
+use Marein\Nchan\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class BearerAuthenticationCredentialsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldExpandTheRequestWithAuthenticationHeader()
+    {
+        $token = 'my-token';
+        $credentials = new BearerAuthenticationCredentials($token);
+
+        $expectedAuthorizationHeaderValue = 'Bearer ' . $token;
+
+        $request = $credentials->authenticate(new Request(
+            'http://localhost',
+            []
+        ));
+
+        $this->assertTrue($request->headers()['Authorization'] === $expectedAuthorizationHeaderValue);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldOverrideAnExistingAuthenticationHeader()
+    {
+        $token = 'my-token';
+        $credentials = new BearerAuthenticationCredentials($token);
+
+        $expectedAuthorizationHeaderValue = 'Bearer ' . $token;
+
+        $request = $credentials->authenticate(new Request(
+            'http://localhost',
+            [
+                'Authorization' => 'Token other-token'
+            ]
+        ));
+
+        $this->assertTrue($request->headers()['Authorization'] === $expectedAuthorizationHeaderValue);
+    }
+}
