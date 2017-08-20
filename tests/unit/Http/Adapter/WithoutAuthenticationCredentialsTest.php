f4d50e160934bd352f5888be98d47b981b294afc
Markus Reinhold <markusreinhold@icloud.com>
2017-08-20 12:28:01 +0200

    Add unit test for WithoutAuthenticationCredentials

---
+++ tests/unit/Http/Adapter/WithoutAuthenticationCredentialsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Marein\Nchan\Http\Adapter;
+
+use Marein\Nchan\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class WithoutAuthenticationCredentialsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldExpandTheRequestWithAuthenticationHeader()
+    {
+        $credentials = new WithoutAuthenticationCredentials();
+
+        $expectedRequest = new Request(
+            'http://localhost',
+            []
+        );
+
+        $request = $credentials->authenticate($expectedRequest);
+
+        $this->assertSame($expectedRequest, $request);
+    }
+}
