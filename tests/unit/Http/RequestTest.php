2145ae8a2ebd01010ab2c48ce2636fe730894170
Markus Reinhold <markusreinhold@icloud.com>
2017-09-14 22:08:09 +0200

    Add unit test for Request

---
+++ tests/unit/Http/RequestTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Marein\Nchan\Http;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithItsValues(): void
+    {
+        $expectedUrl = 'http://localhost/my-url';
+        $expectedHeaders = ['Accept' => 'application/json'];
+        $expectedBody = 'my body';
+
+        $request = new Request($expectedUrl, $expectedHeaders, $expectedBody);
+
+        $this->assertEquals($expectedUrl, $request->url());
+        $this->assertEquals($expectedHeaders, $request->headers());
+        $this->assertEquals($expectedBody, $request->body());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithEmptyBody(): void
+    {
+        $expectedUrl = 'http://localhost/my-url';
+        $expectedHeaders = ['Accept' => 'application/json'];
+        $expectedBody = '';
+
+        $request = new Request($expectedUrl, $expectedHeaders);
+
+        $this->assertEquals($expectedUrl, $request->url());
+        $this->assertEquals($expectedHeaders, $request->headers());
+        $this->assertEquals($expectedBody, $request->body());
+    }
+}
