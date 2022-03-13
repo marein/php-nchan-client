17fd39da5972e14afe79269620d263d2831041c4
Markus Reinhold <markusreinhold@icloud.com>
2022-03-13 12:00:48 +0100

    Pass http factories explicitly #11

--- tests/Integration/HttpAdapter/Psr18ClientAdapterTest.php
+++ tests/Integration/HttpAdapter/Psr18ClientAdapterTest.php
@@ -7,6 +7,7 @@ namespace Marein\Nchan\Tests\Integration\HttpAdapter;
 use Marein\Nchan\Http\Client;
 use Marein\Nchan\Http\Request;
 use Marein\Nchan\HttpAdapter\Psr18ClientAdapter;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpClient\Psr18Client;
 
@@ -14,7 +15,7 @@ class Psr18ClientAdapterTest extends ClientTestCase
 {
     protected function createClient(Request $request): Client
     {
-        $client = new Psr18Client(HttpClient::create());
+        $client = new Psr18Client(HttpClient::create(), new Psr17Factory(), new Psr17Factory());
 
         return new Psr18ClientAdapter(
             $client,
